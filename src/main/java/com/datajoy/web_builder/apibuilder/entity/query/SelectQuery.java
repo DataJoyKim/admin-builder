87f308f78fd16d521da0ab2f282fd70a52b39b34
DataJoyKim <77575923+DataJoyKim@users.noreply.github.com>
2024-10-19 01:12:45 +0900

    1) Entity select delete 쿼리 추가

--- src/main/java/com/datajoy/web_builder/apibuilder/entity/query/SelectQuery.java
+++ src/main/java/com/datajoy/web_builder/apibuilder/entity/query/SelectQuery.java
@@ -2,11 +2,36 @@ package com.datajoy.web_builder.apibuilder.entity.query;
 
 import com.datajoy.web_builder.apibuilder.entity.EntityColumn;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SelectQuery extends AbstractEntityQuery {
     @Override
     public String generateQuery(String tableName, List<EntityColumn> entityColumns) {
-        return null;
+        List<EntityColumn> resolvedEntityColumns = resolveNull(entityColumns);
+
+        List<EntityColumn> whereColumns = new ArrayList<>();
+        List<EntityColumn> orderColumns = new ArrayList<>();
+
+        for(EntityColumn col : resolvedEntityColumns) {
+            if(col.getSelectWhereType() != null) {
+                whereColumns.add(col);
+            }
+
+            if(col.getSelectOrderByNum() != null) {
+                orderColumns.add(col);
+            }
+        }
+
+        orderColumns = orderColumns.stream()
+                .sorted(Comparator.comparing(EntityColumn::getSelectOrderByNum, Comparator.nullsLast(Comparator.naturalOrder())))
+                .collect(Collectors.toList());
+
+        return "select " + System.lineSeparator() +
+                "from " + tableName + System.lineSeparator() +
+                "where 1=1 " + System.lineSeparator() +
+                ";";
     }
 }
