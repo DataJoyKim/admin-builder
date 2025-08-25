46881bdac02d4bb125c6bfa9c0c3a090aa1d769c
DataJoyKim <77575923+DataJoyKim@users.noreply.github.com>
2025-08-25 22:51:47 +0900

    1) console 패키지 이동

--- src/main/java/com/datajoy/admin_builder/console/view/ConsoleViewController.java
+++ src/main/java/com/datajoy/admin_builder/console/view/ConsoleViewController.java
@@ -12,12 +12,12 @@ public class ConsoleViewController {
 
     @GetMapping("")
     public String moveIndex() {
-        return "index";
+        return "console/index";
     }
 
     @GetMapping("/{path}")
     public String moveDataSource(Model model, @PathVariable(name = "path") String path) {
-        model.addAttribute("contentPath","/contents/"+path);
-        return "contents/content";
+        model.addAttribute("contentPath","/console/contents/"+path);
+        return "console/contents/content";
     }
 }
--- src/main/java/com/datajoy/admin_builder/view/ViewBuilderController.java
+++ src/main/java/com/datajoy/admin_builder/view/ViewBuilderController.java
@@ -1,7 +1,13 @@
 package com.datajoy.admin_builder.view;
 
+import com.datajoy.admin_builder.message.ResponseMessage;
+import com.datajoy.admin_builder.security.AuthService;
+import com.datajoy.admin_builder.security.AuthenticatedUser;
+import com.datajoy.admin_builder.security.SecurityBusinessException;
+import com.datajoy.admin_builder.security.TokenUtil;
 import com.datajoy.admin_builder.view.domain.ViewObject;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,12 +22,25 @@ public class ViewBuilderController {
     ObjectMapper objectMapper;
     @Autowired
     ViewObjectService viewObjectService;
+    @Autowired
+    AuthService authService;
     @GetMapping("")
     public String moveAppIndex() {
         return "/pages/index";
     }
     @GetMapping("/pages/{objectCd}")
-    public String moveAppPages(@PathVariable("objectCd") String objectCd) {
+    public String moveAppPages(
+            HttpServletRequest request,
+            @PathVariable("objectCd") String objectCd
+    ) {
+        AuthenticatedUser user = null;
+        try {
+            user = authService.validateAuthentication(TokenUtil.resolveAccessToken(request));
+        }
+        catch (SecurityBusinessException e) {
+            return "/error/login-error";
+        }
+
         ViewObject viewObject = viewObjectService.getViewObject(objectCd);
 
         return "/pages" + viewObject.getPath();
