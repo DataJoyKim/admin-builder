b3cf8bd1e6943e9f2264831f1e84de06205d7b3b
DataJoyKim <77575923+DataJoyKim@users.noreply.github.com>
2025-11-18 00:18:38 +0900

    1) UI 빌더 개선

--- src/main/resources/static/ui/tag/mf/components/Form.js
+++ src/main/resources/static/ui/tag/mf/components/Form.js
@@ -3,12 +3,16 @@ import { AbstractComponents } from './AbstractComponents.js';
 export class Form extends AbstractComponents {
     render(){
         const id = super.getId();
+        let size = this.getAttribute('size');
 
         let className = 'form';
 
         const form = document.createElement('form');
         form.id = id;
         form.className = className;
+        if(size) {
+            form.className += ' ' + size;
+        }
 
         while (this.firstChild) form.appendChild(this.firstChild);
         this.replaceWith(form);
