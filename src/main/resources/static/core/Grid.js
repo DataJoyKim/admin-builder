4e9eedd5ffad2f3031f5e82fa69e2bb3d7bbd3e7
DataJoyKim <77575923+DataJoyKim@users.noreply.github.com>
2025-12-13 17:46:41 +0900

    1) UI 빌더 개선

--- src/main/resources/static/core/Grid.js
+++ src/main/resources/static/core/Grid.js
@@ -9,61 +9,99 @@ class Grid {
           return $("#" + _id).jsGrid("option", "data");
       }
 
+      insertItem(_id, data) {
+          $("#"+_id).jsGrid("insertItem", data);
+      }
+
+        enableRowDrag(id, options) {
+            const $tbody = $("#"+id+" .jsgrid-grid-body tbody");
+
+            let initData = {
+                helper: function (e, ui) {
+                    ui.children().each(function () {
+                        $(this).width($(this).width());
+                    });
+                    return ui;
+                },
+                update:function() {
+                    const newData = [];
+
+                    $("#"+id+" .jsgrid-grid-body tbody tr").each(function () {
+                        newData.push($(this).data("JSGridItem"));
+                    });
+
+                    $("#"+id).jsGrid("option", "data", newData);
+
+                    if(options.updateEvent != undefined) {
+                        options.updateEvent();
+                    }
+                }
+            }
+
+            $tbody.sortable(initData);
+        }
+
       init(_id, _width, _height, _fields, _rowClick, _options) {
-        let sorting = false;
-        let paging = false;
-        let editing = false;
-        let inserting = false;
+        let initData = {
+              height: _height,
+              width: _width,
+              sorting: false,
+              paging: false,
+              editing: false,
+              inserting: false,
+              fields: _fields,
+              data: [],
+              rowClick: function(args) {
+                  _rowClick(args);
+              },
+              rowDoubleClick: function(args) {
+                   $("#" + _id).jsGrid("editItem", args.item);
+              },
+              onItemUpdated: function(args) {
+                  if(_options != undefined && _options.onItemUpdated != undefined) {
+                      _options.onItemUpdated(args);
+                  }
+              },
+              onItemInserting: function(args) {
+                  if(_options != undefined && _options.onItemInserting != undefined) {
+                      _options.onItemInserting(args);
+                  }
+              },
+              onItemDeleting: function(args) {
+                  if(_options != undefined && _options.onItemDeleting != undefined) {
+                      _options.onItemDeleting(args);
+                  }
+              }
+          }
 
         if(_options != undefined) {
             if(_options.sorting != undefined) {
-                sorting = _options.sorting;
+                initData.sorting = _options.sorting;
             }
 
             if(_options.paging != undefined) {
-                paging = _options.paging;
+                initData.paging = _options.paging;
             }
 
             if(_options.editing != undefined) {
-                editing = _options.editing;
+                initData.editing = _options.editing;
             }
 
             if(_options.inserting != undefined) {
-                inserting = _options.inserting;
+                initData.inserting = _options.inserting;
+            }
+
+            if(_options.onRefreshed != undefined) {
+                initData.onRefreshed = _options.onRefreshed;
+            }
+
+            if(_options.loadData != undefined) {
+                initData.controller = initData.controller || {};
+                initData.controller.loadData = _options.loadData;
             }
         }
 
-        $("#" + _id).jsGrid({
-            height: _height,
-            width: _width,
-            sorting: sorting,
-            paging: paging,
-            editing: editing,
-            inserting: inserting,
-            fields: _fields,
-            data: [],
-            rowClick: function(args) {
-                _rowClick(args);
-            },
-            rowDoubleClick: function(args) {
-                 $("#" + _id).jsGrid("editItem", args.item);
-            },
-            onItemUpdated: function(args) {
-                if(_options != undefined && _options.onItemUpdated != undefined) {
-                    _options.onItemUpdated(args);
-                }
-            },
-            onItemInserting: function(args) {
-                if(_options != undefined && _options.onItemInserting != undefined) {
-                    _options.onItemInserting(args);
-                }
-            },
-            onItemDeleting: function(args) {
-                if(_options != undefined && _options.onItemDeleting != undefined) {
-                    _options.onItemDeleting(args);
-                }
-            }
-        });
+        $("#" + _id).jsGrid(initData);
       }
 }
 
